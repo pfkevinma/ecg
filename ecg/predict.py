cd151753f664bba508acd99b296737ba54dc41cf
pfkevinma <70084169+pfkevinma@users.noreply.github.com>
2021-04-14 16:36:15 +0800

    change predict.py

--- ecg/predict.py
+++ ecg/predict.py
@@ -25,10 +25,15 @@ if __name__ == '__main__':
     args = parser.parse_args()
     probs = predict(args.data_json, args.model_path)
     
+    file = open("test.txt", "w")
+    for row in probs:
+        np.savetxt(file, row)
+    file.close()
+    
     # prediction = [sst.mode(np.argmax(prob, axis=2).squeeze())[0][0] for prob in probs]
     # return preproc.int_to_class[prediction]
 
-    for prob in probs:
-        prediction = sst.mode(np.argmax(prob, axis=2).squeeze())[0][0]
-        print(preproc.int_to_class[prediction])
+    # for prob in probs:
+    #     prediction = sst.mode(np.argmax(prob, axis=2).squeeze())[0][0]
+    #     print(preproc.int_to_class[prediction])
 
